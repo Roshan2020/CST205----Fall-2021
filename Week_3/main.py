bdf43d1261d94da68a00c7117693397f27a5f672
Roshan2020 <61339429+Roshan2020@users.noreply.github.com>
2022-02-05 03:50:59 -0800

    Add files via upload

---
+++ Week_3/main.py
@@ -0,0 +1,76 @@
+#------- Lab 2 Task 1 ------
+# Part 1
+color1 = (205, 96, 144)
+
+if color1[0] > color1[1] and color1[0] > color1[2]:
+  print('The color is reddish')
+elif color1[1] > color1[0] and color1[1] > color1[2]:
+  print('The color is greenish')
+else:
+  print('The color is bluish')
+
+# Part 2
+color2 = (28, 134, 238)
+
+if color2[0] > color2[1] and color2[0] > color2[2]:
+  print('The color is reddish')
+elif color2[1] > color2[0] and color2[1] > color2[2]:
+  print('The color is greenish')
+else:
+  print('The color is bluish')
+
+# Part 3
+color3 = (72, 209, 204)
+
+if color3[0] > color3[1] and color3[0] > color3[2]:
+  print('The color is reddish')
+elif color3[1] > color3[0] and color3[1] > color3[2]:
+  print('The color is greenish')
+else:
+  print('The color is bluish')
+
+# Part 4
+color4 = (237, 145, 33)
+
+if color4[0] > color4[1] and color4[0] > color4[2]:
+  print('The color is reddish')
+elif color4[1] > color4[0] and color4[1] > color4[2]:
+  print('The color is greenish')
+else:
+  print('The color is bluish')
+
+#------- Lab 2 Task 2 ------
+# Part 1
+color5 = (250, 250, 70)
+
+if color5[0] == color5[1] and color5[0] > color5[2]:
+  print('The color is a shade of yellow')
+elif color5[0] == color5[2] and color5[0] > color5[1]:
+  print('The color is a shade of magenta')
+elif color5[1] == color5[2] and color5[1] > color5[0]:
+  print('The color is a shade of cyan')
+
+# Part 2
+color6 = (245, 50, 245)
+
+if color6[0] == color6[1] and color6[0] > color6[2]:
+  print('The color is a shade of yellow')
+elif color6[0] == color6[2] and color6[0] > color6[1]:
+  print('The color is a shade of magenta')
+elif color6[1] == color6[2] and color6[1] > color6[0]:
+  print('The color is a shade of cyan')
+
+# Part 3
+color7 = (100, 231, 231)
+
+if color7[0] == color7[1] and color7[0] > color7[2]:
+  print('The color is a shade of yellow')
+elif color7[0] == color7[2] and color7[0] > color7[1]:
+  print('The color is a shade of magenta')
+elif color7[1] == color7[2] and color7[1] > color7[0]:
+  print('The color is a shade of cyan')
+
+# Summary Report on the task above:
+# Once I did one or two parts, I was able to complete both task 1 and 2 without any problems.
+# I had a small issue if I had to mention it would be the constant check to see if I made the right amount of parentesis.
+# In regards to the colour I tried and the result, I used the input examples in the word docx and I ran this file in repl.it and I got the expected result.
